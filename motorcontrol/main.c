c631c65efc2db8f849c9f898fab06311dcde3590
Willi Thiel <ni-c@ni-c.de>
2012-04-18 19:47:52 +0200

    Motorcontrol

--- motorcontrol/main.c
+++ motorcontrol/main.c
@@ -14,17 +14,28 @@
 
 #include <avr/delay.h>
 
+uint8_t running = 0; /*!< If one of the motors is set to another speed then 0, this will change to 1 */
+
 /**
  * The main function.
  */
 int main(void) {
+
+    /* LED on */
+    DDRC |= (1 << DDC6);
+    PORTC |= (1 << PC6);
 
     /* Initialization */
     init_qfly();
 
     /* Our loop */
     while (1) {
-
+        /* Turn LED off if we get a signal */
+        if ((running==0) && (!((i2c_buffer[0]==0) && (i2c_buffer[0]==0) && (i2c_buffer[0]==0) && (i2c_buffer[0]==0)))) {
+            /* LED off */
+            PORTC &= ~(1 << PC6);
+            running=1;
+        }
     }
 
     /* Finally. (Never ever) */
